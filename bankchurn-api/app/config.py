a0fafccd11248195f1f1b2716db92a673214397f
DianaGiraldo <d.giraldoa@uniandes.edu.co>
2026-03-01 14:10:23 -0500

    Update API title with student name

--- bankchurn-api/app/config.py
+++ bankchurn-api/app/config.py
@@ -26,7 +26,7 @@ class Settings(BaseSettings):
         "https://localhost:8000",  # type: ignore
     ]
 
-    PROJECT_NAME: str = "Banckchurn API"
+    PROJECT_NAME: str = "Banckchurn API Diana Giraldo"
 
     class Config:
         case_sensitive = True
